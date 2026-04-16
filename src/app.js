da26f3d53355c9a47544787c7238465766d46901
HTuan <dangvohoangtuan0404@gmail.com>
2026-04-16 20:52:55 +0700

    feat(swagger): setup swagger api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const specs = require("./docs/swagger");
+
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
---
+++ src/docs/swagger.js
@@ -0,0 +1,39 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+
+  apis: ["./src/routes/*.js", "./src/app.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;

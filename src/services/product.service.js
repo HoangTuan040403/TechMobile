26ce990189bd8eae633a83b45a22976f3f2e5b7d
HTuan <dangvohoangtuan0404@gmail.com>
2026-05-22 20:23:21 +0700

    feat(product): implement get detail product API

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -3,6 +3,23 @@ const categoryRepository = require("../repositories/category.repository");
 const MESSAGES = require("../constants/messages");
 const { createError } = require("../utils/error.util");
 
+const formatProduct = (product) => ({
+  _id: product._id,
+  name: product.name,
+  price: product.price,
+  discount: product.discount,
+  price_after_discount: product.price * (1 - product.discount / 100),
+  stock: product.stock,
+  category_id: product.category_id,
+  specs: product.specs,
+  createdAt: product.createdAt
+});
+
+const formatProductDetail = (product) => ({
+  ...formatProduct(product),
+  description: product.description
+});
+
 const createProduct = async ({ name, price, discount, stock, description, category_id, specs }) => {
   const existingName = await productRepository.findByName(name);
   if (existingName) throw createError(MESSAGES.PRODUCT.NAME_ALREADY_EXISTS, 409);
@@ -22,18 +39,7 @@ const createProduct = async ({ name, price, discount, stock, description, catego
     specs: specs || {}
   });
 
-  return {
-    _id: product._id,
-    name: product.name,
-    price: product.price,
-    discount: product.discount,
-    price_after_discount: product.price * (1 - product.discount / 100),
-    stock: product.stock,
-    description: product.description,
-    category_id: product.category_id,
-    specs: product.specs,
-    createdAt: product.createdAt
-  };
+  return formatProductDetail(product);
 };
 
 const getProducts = async (query) => {
@@ -45,20 +51,16 @@ const getProducts = async (query) => {
   const { data, pagination } = await productRepository.findAllProducts({ page, limit, search, category_id });
 
   return {
-    products: data.map((p) => ({
-      _id: p._id,
-      name: p.name,
-      price: p.price,
-      discount: p.discount,
-      price_after_discount: p.price * (1 - p.discount / 100),
-      stock: p.stock,
-      category_id: p.category_id,
-      specs: p.specs,
-      createdAt: p.createdAt
-    })),
+    products: data.map(formatProduct),
     pagination
   };
 };
 
+const getProductById = async (id) => {
+  const product = await productRepository.findById(id);
+  if (!product) throw createError(MESSAGES.PRODUCT.NOT_FOUND, 404);
 
-module.exports = { createProduct, getProducts };
+  return formatProductDetail(product);
+};
+
+module.exports = { createProduct, getProducts, getProductById };
